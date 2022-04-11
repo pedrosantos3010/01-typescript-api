08893042a30e5c458c14fa0af0efb8c2abeec5dd
Pedro Martins <pedrosantos3010@gmail.com>
2022-04-11 19:48:46 -0300

    adicionando controller de usuário

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -7,18 +7,19 @@ const forecast = new Forecast();
 
 @Controller("forecast")
 export class ForecastController {
-  @Get("/")
-  public async getForecastForLoggedUser(
-    _: Request,
-    res: Response
-  ): Promise<void> {
-    try {
-      const beaches = await Beach.find({});
-      const forecastData = await forecast.processForecastForBeaches(beaches);
-      res.status(200).send(forecastData);
-    } catch (e) {
-      console.error(e);
-      res.status(500).send({ error: "Something went wrong" });
+    @Get("/")
+    public async getForecastForLoggedUser(
+        _: Request,
+        res: Response
+    ): Promise<void> {
+        try {
+            const beaches = await Beach.find({});
+            const forecastData = await forecast.processForecastForBeaches(
+                beaches
+            );
+            res.status(200).send(forecastData);
+        } catch (e) {
+            res.status(500).send({ error: "Something went wrong" });
+        }
     }
-  }
 }
--- src/server.ts
+++ src/server.ts
@@ -4,44 +4,50 @@ import express, { Application } from "express";
 import { ForecastController } from "@src/controllers/forecast";
 import * as database from "@src/database";
 import { BeachesController } from "./controllers/beaches";
+import { UserController } from "./controllers/users";
 
 export class SetupServer extends Server {
-  constructor(private port = 3000) {
-    super();
-  }
+    constructor(private port = 3000) {
+        super();
+    }
 
-  public async init(): Promise<void> {
-    this.setupExpress();
-    this.setupControllers();
-    await this.databaseSetup();
-  }
+    public async init(): Promise<void> {
+        this.setupExpress();
+        this.setupControllers();
+        await this.databaseSetup();
+    }
 
-  private setupExpress(): void {
-    this.app.use(express.json());
-    this.setupControllers();
-  }
+    private setupExpress(): void {
+        this.app.use(express.json());
+        this.setupControllers();
+    }
 
-  private setupControllers(): void {
-    const forecastController = new ForecastController();
-    const beachesController = new BeachesController();
-    this.addControllers([forecastController, beachesController]);
-  }
+    private setupControllers(): void {
+        const forecastController = new ForecastController();
+        const beachesController = new BeachesController();
+        const usersController = new UserController();
+        this.addControllers([
+            forecastController,
+            beachesController,
+            usersController,
+        ]);
+    }
 
-  private async databaseSetup(): Promise<void> {
-    await database.connect();
-  }
+    private async databaseSetup(): Promise<void> {
+        await database.connect();
+    }
 
-  public async close(): Promise<void> {
-    await database.close();
-  }
+    public async close(): Promise<void> {
+        await database.close();
+    }
 
-  public getApp(): Application {
-    return this.app;
-  }
+    public getApp(): Application {
+        return this.app;
+    }
 
-  public start(): void {
-    this.app.listen(this.port, () => {
-      console.info("Server listening of port:", this.port);
-    });
-  }
+    public start(): void {
+        this.app.listen(this.port, () => {
+            console.info("Server listening of port:", this.port);
+        });
+    }
 }
